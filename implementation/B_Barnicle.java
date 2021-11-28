716b341e05c874714acaf16699bb81bf7b00b086
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2021-11-28 08:12:07 +0600

    Add files via upload

---
+++ implementation/B_Barnicle.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class B_Barnicle {
+
+    static Scanner in=new Scanner(System.in);
+    
+    public static void main(String[] amit) {
+        
+        System.out.println(in.nextBigDecimal().stripTrailingZeros().toPlainString() );
+        
+    }
+    
+}
+//1.31e1

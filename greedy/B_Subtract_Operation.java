1883ed5ca843111b6bbf58b71b06c85080718dee
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2022-03-24 22:40:20 +0600

    Add files via upload

---
+++ greedy/B_Subtract_Operation.java
@@ -0,0 +1,586 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.StringTokenizer;
+
+public class B_Subtract_Operation {
+
+    static Scanner in = new Scanner();
+
+    static PrintWriter out = new PrintWriter(System.out);
+
+    static int testCases, n;
+
+    static long k;
+
+    static long a[];
+
+    static StringBuilder finalAns = new StringBuilder();
+
+    static void solve(int t) {
+
+        ArrayList<Long> list = new ArrayList<>();
+
+        for (long i : a) {
+
+            list.add(i);
+
+        }
+
+        sort(a, 0, n - 1);
+
+        for (long i : a) {
+
+            long search = i + k;
+
+            int left = 0, right = n - 1;
+
+            while (right >= left) {
+
+                int mid = (left + right) / 2;
+
+                if (a[mid] == search) {
+
+                    if (t == testCases) {
+
+                        finalAns.append("YES");
+
+                        return;
+
+                    }
+
+                    finalAns.append("YES").append("\n");
+
+                    return;
+
+                }
+
+                if (a[mid] < search) {
+
+                    left = mid + 1;
+
+                } else {
+
+                    right = mid - 1;
+
+                }
+
+            }
+
+        }
+
+        if (t == testCases) {
+
+            finalAns.append("NO");
+            return;
+
+        }
+
+        finalAns.append("NO").append("\n");
+
+    }
+
+    public static void main(String[] priya) throws IOException {
+
+        testCases = in.nextInt();
+
+        for (int t = 0; t < testCases; ++t) {
+
+            n = in.nextInt();
+            k = in.nextLong();
+
+            a = new long[n];
+
+            for (int i = 0; i < n; ++i) {
+
+                a[i] = in.nextLong();
+
+            }
+
+            solve(t + 1);
+
+        }
+
+        out.print(finalAns.toString());
+        out.flush();
+
+        in.close();
+
+    }
+
+    static class Node<T> {
+
+        T data;
+
+        Node<T> next;
+
+        public Node() {
+
+            this.next = null;
+
+        }
+
+        public Node(T data) {
+
+            this.data = data;
+
+            this.next = null;
+
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+
+            return this.getData().toString() + "  ";
+
+        }
+
+    }
+
+    static class ArrayList<T> {
+
+        Node<T> head, tail;
+
+        int len;
+
+        public ArrayList() {
+
+            this.head = null;
+
+            this.tail = null;
+
+            this.len = 0;
+
+        }
+
+        int size() {
+
+            return len;
+
+        }
+
+        boolean isEmpty() {
+
+            return len == 0;
+
+        }
+
+        int indexOf(T data) {
+
+            if (isEmpty()) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> temp = head;
+
+            int index = -1, i = 0;
+
+            while (temp != null) {
+
+                if (temp.getData() == data) {
+
+                    index = i;
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            return index;
+
+        }
+
+        void add(T data) {
+
+            Node<T> newNode = new Node<>(data);
+
+            if (isEmpty()) {
+
+                head = newNode;
+
+                tail = newNode;
+
+                len++;
+
+            } else {
+
+                tail.setNext(newNode);
+
+                tail = newNode;
+
+                len++;
+
+            }
+
+        }
+
+        void see() {
+
+            if (isEmpty()) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> temp = head;
+
+            while (temp != null) {
+
+                out.print(temp.getData().toString() + " ");
+                out.flush();
+
+                temp = temp.getNext();
+
+            }
+
+            out.println();
+            out.flush();
+
+        }
+
+        void inserFirst(T data) {
+
+            Node<T> newNode = new Node<>(data);
+
+            Node<T> temp = head;
+
+            if (isEmpty()) {
+
+                head = newNode;
+                tail = newNode;
+
+                len++;
+
+            } else {
+
+                newNode.setNext(temp);
+
+                head = newNode;
+
+                len++;
+
+            }
+
+        }
+
+        T get(int index) {
+
+            if (isEmpty() || index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> temp = head;
+
+            int i = 0;
+
+            T data = null;
+
+            while (temp != null) {
+
+                if (i == index) {
+
+                    data = temp.getData();
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            return data;
+
+        }
+
+        void addAt(T data, int index) {
+
+            if (index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> newNode = new Node<>(data);
+
+            int i = 0;
+
+            Node<T> temp = head;
+
+            while (temp.next != null) {
+
+                if (i == index) {
+
+                    newNode.setNext(temp.next);
+
+                    temp.next = newNode;
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            //    temp.setNext(temp);
+            len++;
+
+        }
+
+        void popFront() {
+
+            if (isEmpty()) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            if (head == tail) {
+
+                head = null;
+                tail = null;
+
+            } else {
+
+                head = head.getNext();
+
+            }
+
+            len--;
+
+        }
+
+        void removeAt(int index) {
+
+            if (index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            if (index == 0) {
+
+                this.popFront();
+
+                return;
+
+            }
+
+            Node<T> temp = head;
+
+            int i = 0;
+
+            Node<T> n = new Node<>();
+
+            while (temp != null) {
+
+                if (i == index) {
+
+                    n.next = temp.next;
+
+                    temp.next = n;
+
+                    break;
+
+                }
+
+                i++;
+
+                n = temp;
+
+                temp = temp.getNext();
+
+            }
+
+            tail = n;
+
+            --len;
+
+        }
+
+        void clearAll() {
+
+            this.head = null;
+            this.tail = null;
+
+        }
+
+    }
+
+    static void merge(long a[], int left, int right, int mid) {
+
+        int n1 = mid - left + 1, n2 = right - mid;
+
+        long L[] = new long[n1];
+
+        long R[] = new long[n2];
+
+        for (int i = 0; i < n1; i++) {
+
+            L[i] = a[left + i];
+
+        }
+
+        for (int i = 0; i < n2; i++) {
+
+            R[i] = a[mid + 1 + i];
+
+        }
+
+        int i = 0, j = 0, k1 = left;
+
+        while (i < n1 && j < n2) {
+
+            if (L[i] <= R[j]) {
+
+                a[k1] = L[i];
+
+                i++;
+
+            } else {
+
+                a[k1] = R[j];
+
+                j++;
+
+            }
+
+            k1++;
+
+        }
+
+        while (i < n1) {
+
+            a[k1] = L[i];
+
+            i++;
+
+            k1++;
+
+        }
+
+        while (j < n2) {
+
+            a[k1] = R[j];
+
+            j++;
+            k1++;
+
+        }
+
+    }
+
+    static void sort(long a[], int left, int right) {
+
+        if (left >= right) {
+
+            return;
+
+        }
+
+        int mid = (left + right) / 2;
+
+        sort(a, left, mid);
+
+        sort(a, mid + 1, right);
+
+        merge(a, left, right, mid);
+
+    }
+
+    static class Scanner {
+
+        BufferedReader in;
+        StringTokenizer st;
+
+        public Scanner() {
+
+            in = new BufferedReader(new InputStreamReader(System.in));
+
+        }
+
+        String next() throws IOException {
+
+            while (st == null || !st.hasMoreElements()) {
+
+                st = new StringTokenizer(in.readLine());
+
+            }
+
+            return st.nextToken();
+
+        }
+
+        String nextLine() throws IOException {
+
+            return in.readLine();
+
+        }
+
+        int nextInt() throws IOException {
+
+            return Integer.parseInt(next());
+
+        }
+
+        double nextDouble() throws IOException {
+
+            return Double.parseDouble(next());
+
+        }
+
+        long nextLong() throws IOException {
+
+            return Long.parseLong(next());
+
+        }
+
+        void close() throws IOException {
+
+            in.close();
+
+        }
+
+    }
+
+}
+/*
+
+4
+4 5
+4 2 2 7
+5 4
+1 9 1 3 4
+2 17
+17 0
+2 17
+18 18
+
+
+ */

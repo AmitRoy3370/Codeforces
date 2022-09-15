6a90428f52c79a5661dc417a98784d6f3f553112
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2022-09-15 18:11:16 +0600

    Add files via upload

---
+++ Constructive algorithom/C_Jumping_on_Tiles.java
@@ -0,0 +1,412 @@
+import java.util.Scanner;
+
+public class C_Jumping_on_Tiles {
+	
+	static Scanner in = new Scanner(System.in);
+	
+	static int testCases, n;
+	
+	static char a[];
+	
+	static void solve() {
+		
+		ArrayList1<Integer> list[] = new ArrayList1[27];
+		
+		for(int i = 0; i <= 26; ++i) {
+			
+			list[i] = new ArrayList1<>();
+			
+		}
+		
+		for(int i = 0; i < n; ++i) {
+			
+			list[a[i] - 'a'].add(i + 1);
+			
+		}
+		
+		long cost = Math.abs(a[n - 1] - a[0]);
+		
+		ArrayList1<Integer> jump = new ArrayList1<>();
+		
+		if(a[n - 1] >= a[0]) {
+			
+			for(int i = (a[0] - 'a'); i <= (a[n - 1] - 'a'); ++i) {
+				
+				while(!list[i].isEmpty()) {
+					
+					jump.add(list[i].get(0));
+					list[i].popFront();
+					
+				}
+				
+			}
+			
+		} else {
+			
+			for(int i = (a[0] - 'a'); i >= (a[n - 1] - 'a'); --i ) {
+				
+				while(!list[i].isEmpty()) {
+					
+					jump.add(list[i].get(0));
+					
+					list[i].popFront();
+					
+				}
+				
+			}
+			
+		}
+		
+		System.out.println(cost + " " + jump.size());
+		
+		jump.see();
+		
+	}
+	
+	public static void main(String [] amit) {
+		
+		testCases = in.nextInt();
+		
+		for(int t = 0; t < testCases; ++t) {
+			
+			a = in.next().toCharArray();
+			
+			n = a.length;
+			
+			solve();
+			
+		}
+		
+	}
+	
+    static class Node<T> {
+
+        T data;
+
+        Node<T> next;
+
+        public Node() {
+
+            this.next = null;
+
+        }
+
+        public Node(T data) {
+
+            this.data = data;
+
+            this.next = null;
+
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+
+            return this.getData().toString() + "  ";
+
+        }
+
+    }
+
+    static class ArrayList1<T> {
+
+        Node<T> head, tail;
+
+        int len;
+
+        public ArrayList1() {
+
+            this.head = null;
+
+            this.tail = null;
+
+            this.len = 0;
+
+        }
+
+        int size() {
+
+            return len;
+
+        }
+
+        boolean isEmpty() {
+
+            return len == 0;
+
+        }
+
+        int indexOf(T data) {
+
+            if (isEmpty()) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> temp = head;
+
+            int index = -1, i = 0;
+
+            while (temp != null) {
+
+                if (temp.getData() == data) {
+
+                    index = i;
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            return index;
+
+        }
+
+        void add(T data) {
+
+            Node<T> newNode = new Node<>(data);
+
+            if (isEmpty()) {
+
+                head = newNode;
+
+                tail = newNode;
+
+                len++;
+
+            } else {
+
+                tail.setNext(newNode);
+
+                tail = newNode;
+
+                len++;
+
+            }
+
+        }
+
+        void see() {
+
+            if (isEmpty()) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> temp = head;
+
+            while (temp != null) {
+
+                System.out.print(temp.getData().toString() + " ");
+                //out.flush();
+
+                temp = temp.getNext();
+
+            }
+
+            System.out.println();
+            //out.flush();
+
+        }
+
+        void inserFirst(T data) {
+
+            Node<T> newNode = new Node<>(data);
+
+            Node<T> temp = head;
+
+            if (isEmpty()) {
+
+                head = newNode;
+                tail = newNode;
+
+                len++;
+
+            } else {
+
+                newNode.setNext(temp);
+
+                head = newNode;
+
+                len++;
+
+            }
+
+        }
+
+        T get(int index) {
+
+            if (isEmpty() || index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            if (index == 0) {
+
+                return head.getData();
+
+            }
+
+            Node<T> temp = head;
+
+            int i = 0;
+
+            T data = null;
+
+            while (temp != null) {
+
+                if (i == index) {
+
+                    data = temp.getData();
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            return data;
+
+        }
+
+        void addAt(T data, int index) {
+
+            if (index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> newNode = new Node<>(data);
+
+            int i = 0;
+
+            Node<T> temp = head;
+
+            while (temp.next != null) {
+
+                if (i == index) {
+
+                    newNode.setNext(temp.next);
+
+                    temp.next = newNode;
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            //    temp.setNext(temp);
+            len++;
+
+        }
+
+        void popFront() {
+
+            if (isEmpty()) {
+
+                //return;
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            if (head == tail) {
+
+                head = null;
+                tail = null;
+
+            } else {
+
+                head = head.getNext();
+
+            }
+
+            len--;
+
+        }
+
+        void removeAt(int index) {
+
+            if (index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            if (index == 0) {
+
+                this.popFront();
+
+                return;
+
+            }
+
+            Node<T> temp = head;
+
+            int i = 0;
+
+            Node<T> n = new Node<>();
+
+            while (temp != null) {
+
+                if (i == index) {
+
+                    n.next = temp.next;
+
+                    temp.next = n;
+
+                    break;
+
+                }
+
+                i++;
+
+                n = temp;
+
+                temp = temp.getNext();
+
+            }
+
+            tail = n;
+
+            --len;
+
+        }
+
+        void clearAll() {
+
+            this.head = null;
+            this.tail = null;
+
+        }
+
+    }
+
+}

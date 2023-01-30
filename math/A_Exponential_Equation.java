35cdf8c197192ac424df34beee0453b5b92ae95a
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2023-01-30 11:43:44 +0600

    B. Number Factorization

--- math/A_Exponential_Equation.java
+++ math/A_Exponential_Equation.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
 
 public class A_Exponential_Equation {
 	
@@ -9,15 +11,83 @@ public class A_Exponential_Equation {
 	
 	static void solve() {
 		
-		if(N % 2 == 1) {
+		Map<Long, Integer> map = new HashMap<>();
+		
+		long max = 1L;
+		
+		long n = N;
+		
+		for(long i = 2L; i * i <= n; ++i) {
 			
-			System.out.println(-1);
+			int count = 0;
 			
-		} else {
-			
-			System.out.println(1 + " " + (N / 2L));
+			if(N % i == 0L) {
+				
+				++count;
+				
+				N /= i;
+				
+				while(N % i == 0L) {
+					
+					++count;
+					N /= i;
+					
+				}
+				
+				map.put(i, count);
+				
+				if(count > max) {
+					
+					max = count;
+					
+				}
+				
+			}
 			
 		}
+		
+		//System.out.println(N);
+		
+		if(N > 1L) {
+			
+			map.put(N, 1);
+			//++max;
+			
+		}
+		
+		long ans = 0L;
+		
+		//System.out.println(max);
+		
+		/*for(long i : map.keySet()) {
+			
+			System.out.println(i + " " + map.get(i));
+			
+		}*/
+		
+		for(long i = 1; i <= max; ++i) {
+			
+			long now = 1L;
+			
+			for(long j : map.keySet()) {
+				
+				if(map.get(j) <= 0) {
+					
+					continue;
+					
+				}
+				
+				now *= j;
+				
+				map.put(j, map.get(j) - 1);
+				
+			}
+			
+			ans += now;
+			
+		}
+		
+		System.out.println(ans);
 		
 	}
 	

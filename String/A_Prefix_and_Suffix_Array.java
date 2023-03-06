0479d22eaf6060b830f620c7c562519fd53538cb
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2023-03-06 15:01:05 +0600

    Add files via upload

---
+++ String/A_Prefix_and_Suffix_Array.java
@@ -0,0 +1,398 @@
+import java.util.Scanner;
+
+public class A_Prefix_and_Suffix_Array {
+	
+	static int testCases, n;
+	
+	static String a[];
+	
+	static Scanner in = new Scanner(System.in);
+	
+	static void solve() {
+		
+		ArrayList1<String> list = new ArrayList1<>();
+		
+		for(String i : a) {
+			
+			if(i.length() == n - 1) {
+				
+				list.add(i);
+				
+			}
+			
+		}
+		
+		String s1 = list.get(1);
+		
+		s1 = new StringBuilder(s1).reverse().toString();
+		
+		if(list.size() >= 2 && list.get(0).equals(s1)) {
+			
+			System.out.println("YES");
+			
+		} else {
+			
+			System.out.println("NO");
+			
+		}
+		
+	}
+	
+	public static void main(String [] priya) {
+		
+		testCases = in.nextInt();
+		
+		for(int t = 0; t < testCases; ++t) {
+			
+			input();
+			solve();
+			
+		}
+		
+	}
+	
+	static void input() {
+		
+		n = in.nextInt();
+			
+		a = new String[2 * n - 2];
+			
+		for(int i = 0; i < 2 * n - 2; ++i) {
+				
+			a[i] = in.next();
+				
+		}
+			
+	}
+	
+    static class Node<T> {
+
+        T data;
+
+        Node<T> next;
+
+        public Node() {
+
+            this.next = null;
+
+        }
+
+        public Node(T data) {
+
+            this.data = data;
+
+            this.next = null;
+
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+        @Override
+        public String toString() {
+
+            return this.getData().toString() + "  ";
+
+        }
+
+    }
+
+    static class ArrayList1<T> {
+
+        Node<T> head, tail;
+
+        int len;
+
+        public ArrayList1() {
+
+            this.head = null;
+
+            this.tail = null;
+
+            this.len = 0;
+
+        }
+
+        int size() {
+
+            return len;
+
+        }
+
+        boolean isEmpty() {
+
+            return len == 0 || head == null || tail == null;
+
+        }
+
+        int indexOf(T data) {
+
+            if (isEmpty()) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> temp = head;
+
+            int index = -1, i = 0;
+
+            while (temp != null) {
+
+                if (temp.getData() == data) {
+
+                    index = i;
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            return index;
+
+        }
+
+        void add(T data) {
+
+            Node<T> newNode = new Node<>(data);
+
+            if (isEmpty()) {
+
+                head = newNode;
+
+                tail = newNode;
+
+                len++;
+
+            } else {
+
+                tail.setNext(newNode);
+
+                tail = newNode;
+
+                len++;
+
+            }
+
+        }
+
+        void see() {
+
+            if (isEmpty()) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> temp = head;
+
+            while (temp != null) {
+
+                System.out.print(temp.getData().toString() + " ");
+                //out.flush();
+
+                temp = temp.getNext();
+
+            }
+
+            System.out.println();
+            //out.flush();
+
+        }
+
+        void inserFirst(T data) {
+
+            Node<T> newNode = new Node<>(data);
+
+            Node<T> temp = head;
+
+            if (isEmpty()) {
+
+                head = newNode;
+                tail = newNode;
+
+                len++;
+
+            } else {
+
+                newNode.setNext(temp);
+
+                head = newNode;
+
+                len++;
+
+            }
+
+        }
+
+        T get(int index) {
+
+            if (isEmpty() || index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            if (index == 0) {
+
+                return head.getData();
+
+            }
+
+            Node<T> temp = head;
+
+            int i = 0;
+
+            T data = null;
+
+            while (temp != null) {
+
+                if (i == index) {
+
+                    data = temp.getData();
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            return data;
+
+        }
+
+        void addAt(T data, int index) {
+
+            if (index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            Node<T> newNode = new Node<>(data);
+
+            int i = 0;
+
+            Node<T> temp = head;
+
+            while (temp.next != null) {
+
+                if (i == index) {
+
+                    newNode.setNext(temp.next);
+
+                    temp.next = newNode;
+
+                }
+
+                i++;
+
+                temp = temp.getNext();
+
+            }
+
+            //    temp.setNext(temp);
+            len++;
+
+        }
+
+        void popFront() {
+
+            if (isEmpty()) {
+
+                //return;
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            if (head == tail) {
+
+                head = null;
+                tail = null;
+
+            } else {
+
+                head = head.getNext();
+
+            }
+
+            len--;
+
+        }
+
+        void removeAt(int index) {
+
+            if (index >= len) {
+
+                throw new ArrayIndexOutOfBoundsException();
+
+            }
+
+            if (index == 0) {
+
+                this.popFront();
+
+                return;
+
+            }
+
+            Node<T> temp = head;
+
+            int i = 0;
+
+            Node<T> n = new Node<>();
+
+            while (temp != null) {
+
+                if (i == index) {
+
+                    n.next = temp.next;
+
+                    temp.next = n;
+
+                    break;
+
+                }
+
+                i++;
+
+                n = temp;
+
+                temp = temp.getNext();
+
+            }
+
+            tail = n;
+
+            --len;
+
+        }
+
+        void clearAll() {
+
+            this.head = null;
+            this.tail = null;
+
+        }
+
+    }
+
+}

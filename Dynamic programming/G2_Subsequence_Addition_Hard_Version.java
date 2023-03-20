6d34375e5cb0d21e4997d6b2f04e21fe427b178c
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2023-03-20 19:53:33 +0600

    Add files via upload

---
+++ Dynamic programming/G2_Subsequence_Addition_Hard_Version.java
@@ -0,0 +1,148 @@
+import java.util.Scanner;
+
+public class G2_Subsequence_Addition_Hard_Version {
+	
+	static int testCases, n;
+	
+	static long a[];
+	
+	static Scanner in = new Scanner(System.in);
+	
+	static void solve() {
+		
+		sort(a, 0, n - 1);
+		
+		long max = 0L;
+		
+		if(a[0] == 1L) {
+			
+			max = 1L;
+			
+		} else {
+			
+			System.out.println("NO");
+			return;
+			
+		}
+		
+		for(int i = 1; i < n; ++i) {
+			
+			if(a[i] > max) {
+				
+				System.out.println("NO");
+				return;
+				
+			}
+			
+			max += a[i];
+			
+		}
+		
+		System.out.println("YES");
+		
+	}
+	
+	public static void main(String [] priya) {
+		
+		testCases = in.nextInt();
+		
+		for(int t = 0; t < testCases; ++t) {
+			
+			n = in.nextInt();
+			
+			a = new long[n];
+			
+			for(int i = 0; i < n; ++i) {
+				
+				a[i] = in.nextLong();
+				
+			}
+			
+			solve();
+			
+		}
+		
+	}
+	
+    static void merge(long a[], int left, int right, int mid) {
+
+        int n1 = mid - left + 1, n2 = right - mid;
+
+        long L[] = new long[n1];
+
+        long R[] = new long[n2];
+
+        for (int i = 0; i < n1; i++) {
+
+            L[i] = a[left + i];
+
+        }
+
+        for (int i = 0; i < n2; i++) {
+
+            R[i] = a[mid + 1 + i];
+
+        }
+
+        int i = 0, j = 0, k1 = left;
+
+        while (i < n1 && j < n2) {
+
+            if (L[i] <= R[j]) {
+
+                a[k1] = L[i];
+
+                i++;
+
+            } else {
+
+                a[k1] = R[j];
+
+                j++;
+
+            }
+
+            k1++;
+
+        }
+
+        while (i < n1) {
+
+            a[k1] = L[i];
+
+            i++;
+
+            k1++;
+
+        }
+
+        while (j < n2) {
+
+            a[k1] = R[j];
+
+            j++;
+            k1++;
+
+        }
+
+    }
+
+    static void sort(long a[], int left, int right) {
+
+        if (left >= right) {
+
+            return;
+
+        }
+
+        int mid = (left + right) / 2;
+
+        sort(a, left, mid);
+
+        sort(a, mid + 1, right);
+
+        merge(a, left, right, mid);
+
+    }
+
+}

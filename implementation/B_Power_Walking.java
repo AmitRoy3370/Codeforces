8eac3c5db94e0a79837e9ef4e6639e5e3d4a937e
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2023-07-29 13:41:30 +0600

    B. Power Walking

---
+++ implementation/B_Power_Walking.java
@@ -0,0 +1,167 @@
+import java.util.Scanner;
+
+public class B_Power_Walking {
+	
+	static Scanner in = new Scanner(System.in);
+	
+	static int testCases, n;
+	
+	static long a[];
+	
+	static void solve() {
+		
+		long final_ans[] = new long[n];
+		
+		sort(a, 0, n - 1);
+		
+		int distinct = 0;
+		
+		for(int i = 0; i < n; ++i) {
+			
+			int j = i + 1;
+			
+			while(i + 1 < n && a[i] == a[i + 1]) {
+				
+				++i;
+				
+			}
+			
+			++distinct;
+			
+			/*if(i != j) {
+			
+			 i = j;
+			
+			}*/
+			
+		}
+		
+		//System.out.println(distinct);
+		
+		for(int i = 1; i <= n; ++i) {
+			
+			final_ans[i - 1] = Math.max(i, distinct);
+			
+		}
+		
+		StringBuilder sb = new StringBuilder(); 
+		
+		for(long i : final_ans) {
+			
+			sb.append(i).append(" ");
+			
+		}
+		
+		System.out.println(sb.toString().trim());
+		
+	}
+	
+	public static void main(String [] priya) {
+		
+		testCases = in.nextInt();
+			
+		for(int t = 0; t < testCases; ++t) {
+			
+			input();
+			solve();
+			
+		}	
+			
+	}
+	
+	private static void input() {
+		
+		n = in.nextInt();
+		
+		a = new long[n];
+		
+		for(int i = 0; i < n; ++i) {
+			
+			a[i] = in.nextLong();
+			
+		}
+		
+	}
+	
+    static void merge(long a[], int left, int right, int mid) {
+ 
+        int n1 = mid - left + 1, n2 = right - mid;
+ 
+        long L[] = new long[n1];
+ 
+        long R[] = new long[n2];
+ 
+        for (int i = 0; i < n1; i++) {
+ 
+            L[i] = a[left + i];
+ 
+        }
+ 
+        for (int i = 0; i < n2; i++) {
+ 
+            R[i] = a[mid + 1 + i];
+ 
+        }
+ 
+        int i = 0, j = 0, k1 = left;
+ 
+        while (i < n1 && j < n2) {
+ 
+            if (L[i] <= R[j]) {
+ 
+                a[k1] = L[i];
+ 
+                i++;
+ 
+            } else {
+ 
+                a[k1] = R[j];
+ 
+                j++;
+ 
+            }
+ 
+            k1++;
+ 
+        }
+ 
+        while (i < n1) {
+ 
+            a[k1] = L[i];
+ 
+            i++;
+ 
+            k1++;
+ 
+        }
+ 
+        while (j < n2) {
+ 
+            a[k1] = R[j];
+ 
+            j++;
+            k1++;
+ 
+        }
+ 
+    }
+ 
+    static void sort(long a[], int left, int right) {
+ 
+        if (left >= right) {
+ 
+            return;
+ 
+        }
+ 
+        int mid = (left + right) / 2;
+ 
+        sort(a, left, mid);
+ 
+        sort(a, mid + 1, right);
+ 
+        merge(a, left, right, mid);
+ 
+    }
+	
+}

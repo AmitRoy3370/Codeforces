a8ba46821dbc109c28f325e33e5c7ee7639e6fd8
AmitRoy3370 <70816613+AmitRoy3370@users.noreply.github.com>
2024-08-17 11:22:02 +0600

    C.  Given Length and Sum of Digits

--- greedy/C_Given_Length_and_Sum_of_Digits.java
+++ greedy/C_Given_Length_and_Sum_of_Digits.java
@@ -1,52 +1,73 @@
 import java.util.Scanner;
 
 public class C_Given_Length_and_Sum_of_Digits {
-
-  static int testCases, n, s;
-  
-  static Scanner in = new Scanner(System.in);
-
-  static void solve() {
-  
-     if((n > 1 && s == 0) || (9 * n < s)) {
-     
-        System.out.print(-1 + " " + (-1));
-        return;
-     
-     }
-  
-     String max = "";
-     String min = "";
-     
-     int sum1 = s - 1;
-     
-     for(int i = 1; i <= n; ++i) {
-     
-         max += (Math.min(9, s));
-         s -= Math.min(9, s);     
-     
-     }
-     
-     for(int i = 1; i < n; ++i) {
-     
-        min = (Math.min(9, sum1)) + min;
-        sum1 -= Math.min(9, sum1);
-     
-     }
-     
-     min = (sum1 + 1) + min;
-     
-     System.out.print(min + " " + max);
-     
-  }
-
-  public static void main(String [] priya) {
-  
-     n = in.nextInt();
-     s = in.nextInt();
-     
-     solve();
-  
-  }
-
+	
+	static Scanner in = new Scanner(System.in);
+	
+	static int testCases, m, s;
+	
+	static void solve() {
+		
+		StringBuilder maxNumber = new StringBuilder();
+		StringBuilder minNumber = new StringBuilder();
+		
+		if((m > 1 && s == 0) || (s > 9 * m)) {
+			
+			minNumber.append("-1");
+			maxNumber.append("-1");
+			
+		} else {
+			
+			for(int i = m - 1, j, k = s; i >= 0; --i) {
+				
+				j = Math.max(0, k - 9 * i);
+				
+				if(j == 0 && i == m - 1 && k > 0) {
+					
+					j = 1;
+					
+				}
+				
+				minNumber.append(j);
+					
+				k -= j;
+					
+			}
+			
+			for(int i = m - 1, j = 0, k = s; i >= 0; --i) {
+				
+				j = Math.min(9, k);
+				
+				maxNumber.append(j);
+				
+				k -= j;
+				
+			}
+			
+		}
+		
+		System.out.println(minNumber.toString() + " " + maxNumber.toString());
+		
+	}
+	
+	public static void main(String [] amit) {
+		
+		testCases = 1;
+		
+		for(int t = 0; t != testCases; ++t) {
+			
+			input();
+			solve();
+			
+		}
+		
+	}
+	
+	private static void input() {
+		
+		m = in.nextInt();
+		s = in.nextInt();
+		
+	}
+	
 }
